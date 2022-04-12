e3a0889d6ab3c7f77cf780705deee5ba93de60d0
yusuke tokumaru <ysr.81115@gmail.com>
2022-04-12 13:16:29 +0900

    s

---
+++ src/main/java/ip/gihyo/projava/tasklist/HomeController.java
@@ -0,0 +1,20 @@
+package ip.gihyo.projava.tasklist;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.time.LocalDateTime;
+
+@Controller
+public class HomeController
+{
+    @RequestMapping(value = "/hello")
+
+    String hello(Model model)
+    {
+        model.addAttribute("time",LocalDateTime.now());
+        return "hello";
+    }
+}
---
+++ src/main/java/ip/gihyo/projava/tasklist/HomeRestController.java
@@ -0,0 +1,48 @@
+package ip.gihyo.projava.tasklist;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RestController
+public class HomeRestController
+{
+    record TaskItem(String id,String task,String deadline,boolean done){
+    }
+    private List<TaskItem> taskItems=new ArrayList<>();
+
+
+    @RequestMapping(value = "/resthello")
+    String hello()
+    {
+        return """
+                Hello.it works! 現在時刻は%sです。
+                """.formatted(LocalDateTime.now());
+    }
+    @GetMapping("/restadd")
+    String addItem(@RequestParam("task") String task,
+                   @RequestParam("deadline") String deadline)
+    {
+        String id= UUID.randomUUID().toString().substring(0,8);
+        TaskItem item=new TaskItem(id,task,deadline,false);
+        taskItems.add(item);
+
+        return "taskを追加しました";
+    }
+    @GetMapping("/restlist")
+    String listItem()
+    {
+        String result= taskItems.stream()
+                .map(TaskItem::toString)
+                .collect(Collectors.joining(","));
+        return result;
+    }
+
+   
+}
